c458ad50ef8503d2002d7358b4b0f52f70f6064f
MeTStIL <stme560@gmail.com>
2025-05-10 14:17:12 +0500

    first fisrts

---
+++ ForTochkaStas/Clients/run.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+class HotelCapacity
+{
+    static bool CheckCapacity(int maxCapacity, List<Guest> guests)
+    {
+        // TODO: реализация алгоритма
+        return true; // или false
+    }
+
+
+    class Guest
+    {
+        public string Name { get; set; }
+        public string CheckIn { get; set; }
+        public string CheckOut { get; set; }
+    }
+
+
+    static void Main()
+    {
+        int maxCapacity = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+
+
+        List<Guest> guests = new List<Guest>();
+
+
+        for (int i = 0; i < n; i++)
+        {
+            string line = Console.ReadLine();
+            Guest guest = ParseGuest(line);
+            guests.Add(guest);
+        }
+
+
+        bool result = CheckCapacity(maxCapacity, guests);
+
+
+        Console.WriteLine(result ? "True" : "False");
+    }
+
+
+    // Простой парсер JSON-строки для объекта Guest
+    static Guest ParseGuest(string json)
+    {
+        var guest = new Guest();
+
+
+        // Извлекаем имя
+        Match nameMatch = Regex.Match(json, "\"name\"\\s*:\\s*\"([^\"]+)\"");
+        if (nameMatch.Success)
+            guest.Name = nameMatch.Groups[1].Value;
+
+
+        // Извлекаем дату заезда
+        Match checkInMatch = Regex.Match(json, "\"check-in\"\\s*:\\s*\"([^\"]+)\"");
+        if (checkInMatch.Success)
+            guest.CheckIn = checkInMatch.Groups[1].Value;
+
+
+        // Извлекаем дату выезда
+        Match checkOutMatch = Regex.Match(json, "\"check-out\"\\s*:\\s*\"([^\"]+)\"");
+        if (checkOutMatch.Success)
+            guest.CheckOut = checkOutMatch.Groups[1].Value;
+
+
+        return guest;
+    }
+}
